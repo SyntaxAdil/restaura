afcb4813bfb1b79bed51644546a7dfe5ae9f758a
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-01-13 02:57:27 +0600

    Hero section complete without fm

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import heroVideo from "../assets/hero.mp4";
+import heroImage from "../assets/hero.jpeg";
+import logo from "../assets/logo.png";
+export const Hero = () => {
+  return (
+    <section className="relative top-0 h-screen w-full ">
+      <video
+        src={heroVideo}
+        autoPlay
+        loop
+        playsInline
+        muted
+        poster={heroImage}
+        className="h-full w-full object-cover"
+      ></video>
+      <div className="inset-0 absolute h-screen w-full overflow-hidden px-0 lg:px-20">
+        <div className="inset-0 absolute bg-linear-to-b from-transparent from-40% to-black z-10"></div>
+        <div className="flex justify-end flex-col relative top-0 h-screen w-full z-20 pb-20 ">
+          <img src={logo} alt="restaura" className="w-full p-4 " />
+          <p className="text-white font-semibold  p-4">Paris</p>
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+  return (
+    <nav className="sticky top-0 mx-auto max-w-7 flex justify-between"> 
+
+    
+
+    </nav>
+  )
+}
