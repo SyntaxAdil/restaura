395bfba99e9aafd625053073064c209f60eebbec
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-01-13 21:03:27 +0600

    Video loading time compress

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,25 @@
 import heroVideo from "../assets/hero.mp4";
 import heroImage from "../assets/hero.jpeg";
 import logo from "../assets/logo.png";
+import { useState } from "react";
 export const Hero = () => {
+  const [videoReady , isVideoReady]=useState(false);
+  
   return (
+
     <section className="relative top-0 h-screen w-full ">
       <video
         src={heroVideo}
-        preload="metadata"
+        preload="auto"
         autoPlay
         loop
         playsInline
         muted
         poster={heroImage}
-        className="h-full w-full object-cover"
+        onCanPlay={()=>{
+          isVideoReady(true)
+        }}
+        className={`h-full w-full object-cover transition-opacity duration-700 ${videoReady ? "opacity-100" : "opacity-1"}`}
       ></video>
       <div className="inset-0 absolute h-screen w-full overflow-hidden px-0 lg:px-20 ">
         <div className="inset-0 absolute bg-linear-to-b from-transparent from-40% to-black z-10"></div>
