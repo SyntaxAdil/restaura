020ac4786bd017a67ad0af0031b1416173f0a689
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-01-13 22:00:18 +0600

    Video fade resolved

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 import heroVideo from "../assets/hero.mp4";
 import heroImage from "../assets/hero.jpeg";
 import logo from "../assets/logo-hero.png";
-import { useState } from "react";
+
 export const Hero = () => {
-  const [videoReady , isVideoReady]=useState(false);
+  
   
   return (
 
@@ -16,14 +16,12 @@ export const Hero = () => {
         playsInline
         muted
         poster={heroImage}
-        onCanPlay={()=>{
-          isVideoReady(true)
-        }}
-        className={`h-full w-full object-cover transition-opacity duration-700 ${videoReady ? "opacity-100" : "opacity-1"}`}
+        
+        className={`h-full w-full object-cover transition-opacity duration-700`}
       ></video>
       <div className="inset-0 absolute h-screen w-full overflow-hidden px-0 lg:px-20 ">
         <div className="inset-0 absolute bg-linear-to-b from-transparent from-40% to-black z-10"></div>
-        <div className={`flex justify-end flex-col relative mx-auto top-0 h-screen lg:w-280 w-full z-20 pb-20 ${videoReady ? "opacity-100" : "opacity-1"}`}>
+        <div className={`flex justify-end flex-col relative mx-auto top-0 h-screen lg:w-280 w-full z-20 pb-20 `}>
           <img src={logo} alt="restaura" className="w-full  px-4 " />
           <p className="text-white font-semibold text-xl -mt-5  px-4">Bangladesh</p>
         </div>
